f9fd1017d3b67bba60880682b9306e7314e8fb79
Kegan Maher <kegan.maher@smgov.net>
2015-01-21 00:47:13 -0800

    clean up and separate concerns

--- Filters/RequireBasicAuthenticationAttribute.cs
+++ Filters/RequireBasicAuthenticationAttribute.cs
@@ -1,55 +1,28 @@
 ﻿using System;
-using System.Linq;
-using System.Web.Http;
-using System.Web.Http.Controllers;
+using System.Web.Mvc;
 using CSM.Security.Services;
 using Orchard;
-using Orchard.Security;
-using Orchard.Security.Permissions;
 
 namespace CSM.Security.Filters
 {
     /// <summary>
-    /// Authorize attribute to enforce HTTP Basic authentication with the provided Orchard permission(s) for access to the decorated controller/action(s).
+    /// Filter to enforce HTTP Basic authentication for the decorated controller/action.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public class RequireBasicAuthorizationAttribute : AuthorizeAttribute
+    public class RequireBasicAuthenticationAttribute : AuthorizeAttribute
     {
-        private readonly string[] _permissionNames;
-
-        public RequireBasicAuthorizationAttribute() 
-            : this(null)
+        public override void OnAuthorization(AuthorizationContext filterContext)
         {
-        }
+            var workContext = filterContext.Controller.ControllerContext.GetWorkContext();
 
-        public RequireBasicAuthorizationAttribute(params string[] permissionNames)
-        {
-            _permissionNames = permissionNames;
-        }
+            var authenticator = workContext.Resolve<IBasicAuthenticationService>();
 
-        protected override bool IsAuthorized(HttpActionContext actionContext)
-        {
-            var workContext = actionContext.ControllerContext.GetWorkContext();
-
-            var user = workContext.Resolve<IBasicAuthenticationService>().GetUserForRequest();
+            var user = authenticator.GetUserForRequest();
 
             if (user == null)
-                return false;
-
-            var authorizationService = workContext.Resolve<IAuthorizationService>();
-
-            if (_permissionNames == null)
-                return true;
-
-            var permissions = _permissionNames.Select(name => new Permission { Name = name });
-
-            foreach (var permission in permissions)
             {
-               if (!authorizationService.TryCheckAccess(permission, user, null))
-                   return false;
+                filterContext.Result = new HttpUnauthorizedResult();
             }
-
-            return true;
         }
     }
 }
---
+++ Filters/RequirePermissionsAttribute.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Orchard;
+using Orchard.Security;
+using Orchard.Security.Permissions;
+
+namespace CSM.Security.Filters
+{
+    /// <summary>
+    /// Filter to require the specified Orchard permission(s) for the decorated controller/action.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class RequirePermissionsAttribute : AuthorizeAttribute
+    {
+        private readonly string[] _permissionNames;
+
+        public RequirePermissionsAttribute(params string[] permissionNames)
+        {
+            _permissionNames = permissionNames;
+        }
+
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            var workContext = filterContext.Controller.ControllerContext.GetWorkContext();
+
+            var user = workContext.CurrentUser;
+
+            if (user == null)
+            {
+                filterContext.Result = new HttpUnauthorizedResult();
+                return;
+            }
+
+            var authorizer = workContext.Resolve<IAuthorizer>();
+
+            var permissions = _permissionNames.Select(name => new Permission { Name = name });
+
+            foreach (var permission in permissions)
+            {
+                if (!authorizer.Authorize(permission))
+                {
+                    filterContext.Result = new HttpUnauthorizedResult();
+                    return;
+                }
+            }
+        }
+    }
+}
