3618e12240a779130ea0f6a610dafd6c7182ebbd
Kegan Maher <kegan.maher@smgov.net>
2015-01-21 00:45:05 -0800

    moving around CORS code

--- Filters/EnableCorsAttribute.cs
+++ Filters/EnableCorsAttribute.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 using System.Linq;
 using System.Web.Http.Filters;
-using CSM.Security.Models;
+using CSM.Security.Services;
 
 namespace CSM.Security.Filters
 {
     /// <summary>
-    /// Action filter attribute to enable the decorated controller/action(s) to be accessed via cross-domain javascript.
+    /// Action filter to enable the decorated controller/action(s) to be accessed via cross-domain javascript.
     /// </summary>
     public class EnableCorsAttribute : ActionFilterAttribute
     {
-        private readonly CorsPolicy _corsPolicy;
+        private readonly ICorsService _corsService;
 
         public EnableCorsAttribute()
             : this("*", "*")
@@ -24,19 +24,7 @@ namespace CSM.Security.Filters
         /// <param name="methods">A comma separated list of allowed HTTP methods (or * to allow any method)</param>
         public EnableCorsAttribute(string origins, string methods)
         {
-            _corsPolicy = new CorsPolicy();
-
-            if (origins == "*")
-                _corsPolicy.AllowAnyOrigin = true;
-
-            if (methods == "*")
-                _corsPolicy.AllowAnyMethod = true;
-
-            string[] methodsArray = methods.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries);
-            string[] originsArray = origins.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries);
-
-            _corsPolicy.AllowedOrigins = originsArray;
-            _corsPolicy.AllowedMethods = methodsArray;
+            _corsService = new CorsService(origins, methods);
         }
 
         /// <summary>
@@ -46,34 +34,24 @@ namespace CSM.Security.Filters
         {
             var request = actionContext.Request;
             var response = actionContext.Response;
+            var requestMethod = request.Method.Method;
 
-            if (_corsPolicy.AllowAnyOrigin && _corsPolicy.AllowAnyMethod)
+            if (_corsService.AllowsAnyOrigin() && _corsService.AllowsMethod(requestMethod))
             {
                 response.Headers.Add("Access-Control-Allow-Origin", "*");
                 return;
             }
 
-            if(!request.Headers.Contains("Origin"))
-                return;
-
             var requestOrigin = request.Headers.GetValues("Origin").FirstOrDefault();
-            var requestMethod = request.Method.Method;
 
             if (!String.IsNullOrEmpty(requestOrigin))
             {
-                bool originAllowed = 
-                    _corsPolicy.AllowAnyOrigin ||
-                    _corsPolicy.AllowedOrigins.Any(o => o.Equals(requestOrigin, StringComparison.OrdinalIgnoreCase));
-
-                bool methodAllowed =
-                    _corsPolicy.AllowAnyMethod ||
-                    _corsPolicy.AllowedMethods.Any(m => m.Equals(requestMethod, StringComparison.OrdinalIgnoreCase));
-
-                if (originAllowed && methodAllowed)
+                if (_corsService.AllowsOrigin(requestOrigin) &&
+                    _corsService.AllowsMethod(requestMethod))
+                {
                     response.Headers.Add("Access-Control-Allow-Origin", new[] { requestOrigin });
+                }
             }
-
-            base.OnActionExecuted(actionContext);
         }
     }
 }
---
+++ Services/ICorsService.cs
@@ -0,0 +1,12 @@
+﻿using Orchard;
+
+namespace CSM.Security.Services
+{
+    public interface ICorsService : IDependency
+    {
+        bool AllowsAnyOrigin();
+        bool AllowsOrigin(string origin);
+        bool AllowsAnyMethod();
+        bool AllowsMethod(string method);
+    }
+}
